104b933415ca7d7d3f96945745a823d05e076fb2
gys619 <1554537386@qq.com>
2022-02-07 00:06:55 +0800

    新增：[ backUp/jd_speed_redpocke.js, gua_cleancart_ddo.js] 删除：[ gua_opencard93.js, gua_opencard95.js] 修改内容：[ config.json, jd_ddworld_exchange.js, jd_speed_redpocke.js, sendNotify.js]

--- sendNotify.js
+++ sendNotify.js
@@ -199,7 +199,7 @@ async function sendNotify(
   text,
   desp,
   params = {'TG交流群':'https://t.me/jd_zero_205'},
-  author = '\n\n云函数老用户这个月开始实行新收费政策,请自行去自己私库页面或查看群内置顶.不需要请及时删除函数和github私库.',
+  author = '\n\n请更新拉库命令:https://github.com/888888/JD_tencent_scf/tree/main',
 ) {
   let no_notify = process.env.no_notify
   if (no_notify) {
